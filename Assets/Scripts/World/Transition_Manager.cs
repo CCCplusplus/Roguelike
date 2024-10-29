c7bccedc01c23cfbe529893b7bcd69a70698ee2d
CCCplusplus <139177589+CCCplusplus@users.noreply.github.com>
2024-10-29 03:21:36 -0600

    Inicio de UI

---
+++ Assets/Scripts/World/Transition_Manager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition_Manager : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    public void LoadGameover()
+    {
+        SceneManager.LoadSceneAsync("GameOver");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadSceneAsync("GamePlay");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Stop!");
+    }
+}
